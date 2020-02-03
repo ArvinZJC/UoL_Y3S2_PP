7498f2b478fdc5ca92f5c93e224733357c47e376
ArvinZJC <zjcarvin@outlook.com>
2020-02-03 17:11:38 +0000

    Update the enumerator from "enum" to "enum class" in the utils because of type safety.

--- W1-8/include/Utils.h
+++ W1-8/include/Utils.h
@@ -15,32 +15,39 @@
 using namespace std;
 
 template <typename T>
-ostream& operator<< (ostream& out, const vector<T>& v) {
-	if (!v.empty()) {
+ostream& operator<< (ostream& out, const vector<T>& v)
+{
+	if (!v.empty())
+	{
 		out << '[';
 		copy(v.begin(), v.end(), ostream_iterator<T>(out, ", "));
 		out << "\b\b]";
-	}
-	return out;
-}
+	} // end if
 
-string GetPlatformName(int platform_id) {
+	return out;
+} // end overloading the << operator
+
+string GetPlatformName(int platform_id)
+{
 	vector<cl::Platform> platforms;
 	cl::Platform::get(&platforms);
 	return platforms[platform_id].getInfo<CL_PLATFORM_NAME>();
-}
+} // end function GetPlatformName
 
-string GetDeviceName(int platform_id, int device_id) {
+string GetDeviceName(int platform_id, int device_id)
+{
 	vector<cl::Platform> platforms;
 	cl::Platform::get(&platforms);
 	vector<cl::Device> devices;
 	platforms[platform_id].getDevices((cl_device_type)CL_DEVICE_TYPE_ALL, &devices);
 	return devices[device_id].getInfo<CL_DEVICE_NAME>();
-}
+} // end function GetDeviceName
 
-const char *getErrorString(cl_int error) {
-	switch (error){
-		// run-time and JIT compiler errors
+const char *getErrorString(cl_int error)
+{
+	switch (error)
+	{
+	// run-time and JIT compiler errors
 	case 0: return "CL_SUCCESS";
 	case -1: return "CL_DEVICE_NOT_FOUND";
 	case -2: return "CL_DEVICE_NOT_AVAILABLE";
@@ -62,7 +69,7 @@ const char *getErrorString(cl_int error) {
 	case -18: return "CL_DEVICE_PARTITION_FAILED";
 	case -19: return "CL_KERNEL_ARG_INFO_NOT_AVAILABLE";
 
-		// compile-time errors
+	// compile-time errors
 	case -30: return "CL_INVALID_VALUE";
 	case -31: return "CL_INVALID_DEVICE_TYPE";
 	case -32: return "CL_INVALID_PLATFORM";
@@ -103,7 +110,7 @@ const char *getErrorString(cl_int error) {
 	case -67: return "CL_INVALID_LINKER_OPTIONS";
 	case -68: return "CL_INVALID_DEVICE_PARTITION_COUNT";
 
-		// extension errors
+	// extension errors
 	case -1000: return "CL_INVALID_GL_SHAREGROUP_REFERENCE_KHR";
 	case -1001: return "CL_PLATFORM_NOT_FOUND_KHR";
 	case -1002: return "CL_INVALID_D3D10_DEVICE_KHR";
@@ -111,25 +118,28 @@ const char *getErrorString(cl_int error) {
 	case -1004: return "CL_D3D10_RESOURCE_ALREADY_ACQUIRED_KHR";
 	case -1005: return "CL_D3D10_RESOURCE_NOT_ACQUIRED_KHR";
 	default: return "Unknown OpenCL error";
-	}
-}
+	} // end switch-case
+} // end function getErrorString
 
-void CheckError(cl_int error) {
-	if (error != CL_SUCCESS) {
+void CheckError(cl_int error)
+{
+	if (error != CL_SUCCESS)
+	{
 		cerr << "OpenCL call failed with error " << getErrorString(error) << endl;
 		exit(1);
-	}
-}
+	} // end if
+} // end function CheckError
 
-void AddSources(cl::Program::Sources& sources, const string& file_name) {
-	//TODO: add file existence check
+void AddSources(cl::Program::Sources& sources, const string& file_name)
+{
+	// TODO: add file existence check
 	ifstream file(file_name);
 	string* source_code = new string(istreambuf_iterator<char>(file), (istreambuf_iterator<char>()));
 	sources.push_back((*source_code).c_str());
-}
+} // end function AddSources
 
-string ListPlatformsDevices() {
-
+string ListPlatformsDevices()
+{
 	stringstream sstream;
 	vector<cl::Platform> platforms;
 
@@ -142,7 +152,7 @@ string ListPlatformsDevices() {
 		sstream << "\nPlatform " << i << ", " << platforms[i].getInfo<CL_PLATFORM_NAME>() << ", version: " << platforms[i].getInfo<CL_PLATFORM_VERSION>();
 
 		sstream << ", vendor: " << platforms[i].getInfo<CL_PLATFORM_VENDOR>() << endl;
-		//		sstream << ", extensions: " << platforms[i].getInfo<CL_PLATFORM_EXTENSIONS>() << endl;
+		// sstream << ", extensions: " << platforms[i].getInfo<CL_PLATFORM_EXTENSIONS>() << endl;
 
 		vector<cl::Device> devices;
 
@@ -157,28 +167,35 @@ string ListPlatformsDevices() {
 			sstream << ", vendor: " << devices[j].getInfo<CL_DEVICE_VENDOR>();
 			cl_device_type device_type = devices[j].getInfo<CL_DEVICE_TYPE>();
 			sstream << ", type: ";
+
 			if (device_type & CL_DEVICE_TYPE_DEFAULT)
 				sstream << "DEFAULT ";
+
 			if (device_type & CL_DEVICE_TYPE_CPU)
 				sstream << "CPU ";
+
 			if (device_type & CL_DEVICE_TYPE_GPU)
 				sstream << "GPU ";
+
 			if (device_type & CL_DEVICE_TYPE_ACCELERATOR)
 				sstream << "ACCELERATOR ";
+
 			sstream << ", compute units: " << devices[j].getInfo<CL_DEVICE_MAX_COMPUTE_UNITS>();
 			sstream << ", clock freq [MHz]: " << devices[j].getInfo<CL_DEVICE_MAX_CLOCK_FREQUENCY>();
 			sstream << ", max memory size [B]: " << devices[j].getInfo<CL_DEVICE_GLOBAL_MEM_SIZE>();
 			sstream << ", max allocatable memory [B]: " << devices[j].getInfo<CL_DEVICE_MAX_MEM_ALLOC_SIZE>();
 
 			sstream << endl;
-		}
-	}
+		} // end for
+	} // end for
+
 	sstream << "----------------------------------------------------------------" << endl;
 
 	return sstream.str();
-}
+} // end function ListPlatformsDevices
 
-cl::Context GetContext(int platform_id, int device_id) {
+cl::Context GetContext(int platform_id, int device_id)
+{
 	vector<cl::Platform> platforms;
 
 	cl::Platform::get(&platforms);
@@ -189,37 +206,30 @@ cl::Context GetContext(int platform_id, int device_id) {
 		platforms[i].getDevices((cl_device_type)CL_DEVICE_TYPE_ALL, &devices);
 
 		for (unsigned int j = 0; j < devices.size(); j++)
-		{
 			if ((i == platform_id) && (j == device_id))
 				return cl::Context({ devices[j] });
-		}
-	}
+	} // end for
 
 	return cl::Context();
-}
+} // end function GetContext
 
-enum ProfilingResolution {
+enum class ProfilingResolution
+{
 	PROF_NS = 1,
 	PROF_US = 1000,
 	PROF_MS = 1000000,
 	PROF_S = 1000000000
 };
 
-string GetFullProfilingInfo(const cl::Event& evnt, ProfilingResolution resolution) {
+string GetFullProfilingInfo(const cl::Event& evnt, ProfilingResolution resolutionSelection)
+{
 	stringstream sstream;
+	auto resolution = static_cast<int>(resolutionSelection);
 
 	sstream << "Queued " << (evnt.getProfilingInfo<CL_PROFILING_COMMAND_SUBMIT>() - evnt.getProfilingInfo<CL_PROFILING_COMMAND_QUEUED>()) / resolution;
 	sstream << ", Submitted " << (evnt.getProfilingInfo<CL_PROFILING_COMMAND_START>() - evnt.getProfilingInfo<CL_PROFILING_COMMAND_SUBMIT>()) / resolution;
 	sstream << ", Executed " << (evnt.getProfilingInfo<CL_PROFILING_COMMAND_END>() - evnt.getProfilingInfo<CL_PROFILING_COMMAND_START>()) / resolution;
 	sstream << ", Total " << (evnt.getProfilingInfo<CL_PROFILING_COMMAND_END>() - evnt.getProfilingInfo<CL_PROFILING_COMMAND_QUEUED>()) / resolution;
 
-	switch (resolution) {
-	case PROF_NS: sstream << " [ns]"; break;
-	case PROF_US: sstream << " [us]"; break;
-	case PROF_MS: sstream << " [ms]"; break;
-	case PROF_S: sstream << " [s]"; break;
-	default: break;
-	}
-
 	return sstream.str();
-}
+} // end function GetFullProfilingInfo
