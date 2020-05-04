c090b978cb1de7e6f1cf39b8ed8b0250e3d3bd2b
ArvinZJC <zjcarvin@outlook.com>
2020-05-04 13:31:02 +0100

    Learn Tutorial 4.

--- Tutorial 4/Tutorial 4.cpp
+++ Tutorial 4/Tutorial 4.cpp
@@ -7,8 +7,16 @@
 namespace compute = boost::compute;
 using namespace std;
 
-int main() {
+int main()
+{
 	typedef int mytype;
+
+	// get default device and setup context
+	compute::device device = compute::system::default_device();
+	compute::context context(device);
+	compute::command_queue queue(context, device);
+
+	cout << "Running on " << device.name() << endl;
 
 	// create vectors on the host
 	vector<mytype> A = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
@@ -16,23 +24,30 @@ int main() {
 	vector<mytype> C(A.size());
 
 	// create vectors on the device
-	compute::vector<mytype> devA(A.size());
-	compute::vector<mytype> devB(B.size());
-	compute::vector<mytype> devC(C.size());
+	// compute::vector<mytype> devA(A.size());
+	compute::vector<mytype> devA(A.size(), context);
+	// compute::vector<mytype> devB(B.size());
+	compute::vector<mytype> devB(B.size(), context);
+	// compute::vector<mytype> devC(C.size());
+	compute::vector<mytype> devC(C.size(), context);
 
 	// copy input data to the device
-	compute::copy(A.begin(), A.end(), devA.begin());
-	compute::copy(B.begin(), B.end(), devB.begin());
+	// compute::copy(A.begin(), A.end(), devA.begin());
+	compute::copy(A.begin(), A.end(), devA.begin(), queue);
+	// compute::copy(B.begin(), B.end(), devB.begin());
+	compute::copy(B.begin(), B.end(), devB.begin(), queue);
 
-	// perform C = A + B
-	compute::transform(devA.begin(), devA.end(), devB.begin(), devC.begin(), compute::plus<mytype>());
+	// perform C = A + B (use the map pattern)
+	// compute::transform(devA.begin(), devA.end(), devB.begin(), devC.begin(), compute::plus<mytype>());
+	compute::transform(devA.begin(), devA.end(), devB.begin(), devC.begin(), compute::plus<mytype>(), queue);
 
 	// copy data back to the host
-	compute::copy(devC.begin(), devC.end(), C.begin());
+	// compute::copy(devC.begin(), devC.end(), C.begin());
+	compute::copy(devC.begin(), devC.end(), C.begin(), queue);
 
 	cout << "A = " << A << endl;
 	cout << "B = " << B << endl;
 	cout << "C = " << C << endl;
 
 	return 0;
-}
+} // end main
